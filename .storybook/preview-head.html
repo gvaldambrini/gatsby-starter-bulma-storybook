e43c0f9ae5a0d765262d3e55a253370dac5c9d73
Gianni Valdambrini <aleister@develer.com>
2018-04-07 16:12:34 +0200

    Add font-awesome fonts.

---
+++ .storybook/preview-head.html
@@ -0,0 +1,1 @@
+<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Link from 'gatsby-link';
 
 const IndexPage = () => (
   <div className="content">
-    <h1>Hi people</h1>
+    <h1>
+      Hi people <i className="fas fa-exclamation-circle" />
+    </h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
